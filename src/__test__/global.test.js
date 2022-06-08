42c7bf8392f79579a88dca3a063a0e8bea76a021
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-06-07 21:37:13 -0500

    learn how to create more tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,10 @@
 const text = 'Hello World';
+const fourElements = ['💦', '🪨', '🔥', '💨'];
 
 test('It should be a test', () => {
     expect(text).toMatch(/World/);
+});
+
+test('We have fire', () => {
+    expect(fourElements).toContain('🔥');
 });
